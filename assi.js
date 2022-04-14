4f5c702dca3ece1c79562f77db9b5e6809e60007
cjwire <johnsamuelisong@gmail.com>
2022-04-14 09:16:55 +0100

    message

---
+++ assi.js
@@ -0,0 +1,54 @@
+// let i = 3;
+// while (i) {
+//  alert( i-- );
+// }
+
+
+
+// let i = 0;
+// while (++i < 5)
+//  alert( i );
+
+
+// let b = 0;
+// while (b< 5) alert( b++ );
+
+ 
+
+// let num = prompt("enter value");
+// num =1
+// while(num <= 100){
+//     alert(num )
+// }
+
+
+// for (let i = 0; i < 3; i++) {
+//     alert( `number ${i}!` );
+//    } 
+   
+//    let i =0
+//    while(i<3){
+//     alert (`number  ${i}`)
+
+//     i++
+//    }
+
+// let num = 100;
+//  do{
+//      num =prompt ('Enter Number Greater THan 100')
+
+//  } while(num <=100){
+//     alert ('You Got the right one')
+//  }
+
+
+// let n = 10;
+// prime: 
+// for (let i =2; i<= n; i++){
+//     for (let x = 2; x <i; x++){
+//         if (i % x ==0)continue prime;
+//     }
+//     alert(i);
+// }
+
+// {alert(' to be continue press ok to see the magin')}
